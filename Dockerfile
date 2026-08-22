60c2c493a270292e64ed7213a1d30e7d7524f05f
Ducky <124077142+ducky4life@users.noreply.github.com>
2026-08-22 22:15:25 +0800

    basic usage

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,10 @@ RUN python -m pip install --upgrade pip
 
 RUN pip install -r requirements.txt
 
+RUN apt-get update -y && apt-get install -y chromium
+
+RUN echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --no-sandbox"' >> /etc/chromium.d/default-flags
+
 COPY ferg.py .env /
 
 WORKDIR /
--- ferg.py
+++ ferg.py
@@ -3,15 +3,25 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+from html2image import Html2Image
+import time
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 load_dotenv()
 
-bot_prefix = "smort"
+bot_prefix = "ferg"
+output_path = "images"
 
-token = os.getenv("ROBO_TOKEN")
+token = os.getenv("FERG_TOKEN")
+hti = Html2Image(
+    output_path=output_path,
+    custom_flags=['--virtual-time-budget=10000', '--hide-scrollbars', '--default-background-color=00000000'],
+)
+
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
 
 client = commands.Bot(
     command_prefix=[f"!{bot_prefix} "],
@@ -42,14 +52,36 @@ async def send_codeblock(ctx, msg, *, view=None):
     else:
         await ctx.send(f"```{msg}```", view=view)
 
-bot_id_list = [1186326404267266059, 839794863591260182, 944245571714170930, 1396935480284680334]
+
+@client.hybrid_command()
+async def load_html(ctx, *, html: str):
+    curr_time = int(time.time())
+    hti.screenshot(html_str=html, css_str="", save_as=f'{curr_time}.png')
+    await ctx.send("ok", file=discord.File(f'{output_path}/{curr_time}.png'))
+
+@client.hybrid_command()
+async def load_html_url(ctx, url: str):
+    curr_time = int(time.time())
+    hti.screenshot(url=url, save_as=f'{curr_time}.png')
+    await ctx.send("ok", file=discord.File(f'{output_path}/{curr_time}.png'))
+
+@client.hybrid_command()
+async def load_html_file(ctx, html_file: discord.Attachment):
+    await html_file.save()
+    curr_time = int(time.time())
+    hti.screenshot(html_file=html_file.filename, save_as=f'{curr_time}.png')
+    os.remove(html_file.filename)
+    await ctx.send("ok", file=discord.File(f'{output_path}/{curr_time}.png'))
+    
+
+bot_id_list = [1186326404267266059, 839794863591260182, 944245571714170930, 1396935480284680334, 1414634216292876308, 1529839995639173320]
 
 @client.event
 async def on_message(message: discord.Message):
     await client.process_commands(message)
-    if message.author.id not in bot_id_list and message.author.id:
-        if "baa" in message.content.lower():
-            await message.channel.send("Baaaaaaaa!")
+    if message.author.id not in bot_id_list and message.author.id != client.application_id:
+        if "ugli" in message.content.lower():
+            await message.channel.send("hi i ugli")
 
 @client.event
 async def on_command_error(ctx, error):
